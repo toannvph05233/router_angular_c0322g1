b7a38b5b2cf0a4d24fe590884e4dc6fd7a3bbfd5
toannvph05233 <johntoan98@gmail.com>
2022-08-23 10:35:22 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CreateComponent} from "./product/create/create.component";
+import {ShowComponent} from "./product/show/show.component";
+import {AuthGuard} from "./auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: '', component: ShowComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowComponent } from './product/show/show.component';
+import { CreateComponent } from './product/create/create.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowComponent,
+    CreateComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
